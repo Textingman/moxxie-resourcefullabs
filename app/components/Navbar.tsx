dab866a32bb450b484c648d1409892c352cabfc8
Textingman <matt.morf@textvolt.com>
2026-07-01 13:48:29 -0400

    Initial site setup: Resourceful Labs

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
                 Terms
               </Link>
               <Link href="/signup" className="text-white px-4 py-2 rounded-md text-sm font-medium" style={{ backgroundColor: '#059669' }}>
-                Get Guided
+                Talk to a Consultant
               </Link>
             </div>
           </div>
